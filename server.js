b51db4f1971d9a82c676a12da04c245010f66229
Linawaty <linawatylim@gmail.com>
2020-07-18 15:45:07 +0800

    Set the backend

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 // Dependencies
-const express = require('express');
+const express = require('express')
+const mongoose = require('mongoose')
 const app = express();
-const PORT = 3000;
+
+app.use(express.json());
+
+// Environment Variables (getting ready for Heroku)
+const PORT = process.env.PORT || 3000;
 
 // Routes
-app.get('/', (req, res) => {
-    console.log(`Get request from /, sent response.`);
-    console.log(req.params);
-    console.log(req.query);
-    res.send('ToDo in React Express Mongoose');
-});
+const todosController = require('./controllers/todos.js');
+app.use('/todos', todosController);
+
+const db = mongoose.connection;
+
+// Environment Variables
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://dbLina:jBdf48k6DlIEp3ZK@clstrlin001-ldhm1.mongodb.net/todo?retryWrites=true&w=majority'
+
+// Connect to Mongo
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log('MongoDB connection established:', mongoURI)
+)
+
+// Error / Disconnection
+db.on('error', err => console.log(err.message + ' is Mongod not running?'));
+db.on('disconnected', () => console.log('mongo disconnected'));
 
 // App Listen at the last
 app.listen(PORT, () => {
